16397798b8075548927110d70d40bbd1d078180f
EC2 Default User <ec2-user@ip-172-31-57-20.us-west-2.compute.internal>
2024-06-20 05:37:40 +0000

    update

--- 1-extract-presenter-notes.py
+++ 1-extract-presenter-notes.py
@@ -3,6 +3,7 @@ import collections.abc
 from pptx import Presentation
 import re
 import sys
+import os.path
 
 if len(sys.argv) == 2:
         input_file = sys.argv[1]
@@ -12,7 +13,11 @@ else:
 
 ppt=Presentation(input_file)
 notes = []
+filename=os.path.basename(input_file)
+output_file=filename[:-5]+"_presenter_notes.md"
 
+f = open(output_file,"w")
+f.write("# {} presenter notes and links".format(filename))
 for page, slide in enumerate(ppt.slides):
     # this is the notes that doesn't appear on the ppt slide,
     # but really the 'presenter' note. 
@@ -29,11 +34,13 @@ for page, slide in enumerate(ppt.slides):
             # slide does not contain any text, only title:
             text_frame_text=""
         
-    print("\n-------\n### Slide {}: {}\n\n{}".format(int(page)+1, text_frame_text, text_notes))
+    f.write("\n\n-------\n### Slide {}: {}\n\n{}\n".format(int(page)+1, text_frame_text, text_notes))
     # Use re.findall() to extract all matches
     https_links = re.findall(pattern, text_notes)
     if len(https_links) > 0:
-        print("\nHTTPS links found:")
+        f.write("\nHTTPS links found:\n")
         for link in https_links:
             cleaned_link = re.sub(r'[^/\w\d]+$', '', link)
-            print("* {}".format(cleaned_link))
+            f.write("* {}\n".format(cleaned_link))
+
+f.close()
